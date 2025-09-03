f5a49342983021fc5b6f07d41e9e4efe6c4ac244
Mauricio Sosa Giri <gm@free4fun.dev>
2025-09-03 10:01:51 -0300

    feat: add UseGTMPageView component to index and update App import path

--- index.tsx
+++ index.tsx
@@ -1,7 +1,8 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from "@/App";
+import App from "./src/App";
+import UseGTMPageView from "./src/components/UseGTMPageView";
 import './src/index.css';
 
 const rootElement = document.getElementById('root');
@@ -12,6 +13,8 @@ if (!rootElement) {
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
+    <UseGTMPageView />
+
     <App />
   </React.StrictMode>
 );
